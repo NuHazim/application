91917a9b9024c6fa8e8cfa9de6162515334a0db0
sarif.ishak@gmail.com <sarif.ishak@gmail.com>
2024-12-25 23:48:39 +0800

    Start simple

--- rfidReceiver.php
+++ rfidReceiver.php
@@ -1,1 +1,17 @@
-//testing
+<?php
+include("database.inc");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Application</title>
+    <link rel="stylesheet" href="styleclient.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Application form</h1>
+</div>
+</body>
+</html>
