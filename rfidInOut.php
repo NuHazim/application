0b98b29789e657c15c76c7f9ad0eeaaadc81a9c3
sarif.ishak@gmail.com <sarif.ishak@gmail.com>
2024-12-26 08:20:02 +0800

    basic code

--- rfidInOut.php
+++ rfidInOut.php
@@ -1,10 +1,10 @@
 <?php
 include("database.inc");
-if (isset($_POST['action'])) {
+if (isset($_GET['action'])) {
     // Retrieve input values
-    $action = $_POST['action'];
-    $rfid = $_POST['rfid'];
-    echo "action is " + $action;
+    $action = $_GET['action'];
+    $rfid = $_GET['rfid'];
+    echo "action is " . $action;
 } else {
   echo "No post value of post";
 
