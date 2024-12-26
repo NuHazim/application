11f28dadc3bf9f881a18bfb2a09797cb3a1df94d
sarif.ishak@gmail.com <sarif.ishak@gmail.com>
2024-12-26 10:25:31 +0800

    change header to json output

--- gettable.php
+++ gettable.php
@@ -20,7 +20,7 @@ include("database.inc");
     }
 
     $jsonString=$jsonString."]";
-    echo $jsonString;
-
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($jsonString);
 
 ?>
