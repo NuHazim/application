83c000b6154890ce264ae4c2229c052caedb4645
sarif.ishak@gmail.com <sarif.ishak@gmail.com>
2024-12-26 08:57:35 +0800

    Display name

--- rfidInOut.php
+++ rfidInOut.php
@@ -5,8 +5,21 @@ if (isset($_GET['action'])) {
     $action = $_GET['action'];
     $rfid = $_GET['rfid'];
     echo "action is " . $action;
+    $sql2 = "SELECT * FROM userlist WHERE Access='".rfid."'";
+    $stmt = $pdo->query($sql2); // Execute query and fetch results
+
+    if ($stmt->rowCount() > 0) {
+        
+        //while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        //}
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $name = $row["Name"];
+        echo "Record for rfid ". $rfid. " name is ".$name;
+    } else {
+        echo "No record found for rfid ". $rfid;
+    }
 } else {
-  echo "No post value of post";
+  echo "No post value of action and rfid parameter";
 
 }
 ?>
